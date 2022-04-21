d3bac18a39c9ebbbc803c69e7b37491aa5be36e9
kmiller <kmmiller418@gmail.com>
2022-04-21 16:49:13 -0400

    Complete form/about us page

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,10 +16,11 @@ const Form = () => {
   };
 
   const handleSubmit = () => {
-    if (!feedback.name || !feedback.email || !feedback.message) {
-      alert('Please fill out all required fields');
+    if (feedback.name && feedback.email && feedback.message) {
+      updateFeedback({ valid: true });
       return;
     }
+    alert("please fill out all required fields, marked with an asterisk");
   };
 
   const initialFeedback = {
@@ -27,7 +28,9 @@ const Form = () => {
     email: "",
     tel: "",
     message: "",
+    valid: false,
   };
+  
   const [feedback, updateFeedback] = useReducer(
     (feedback, updates) => ({
       ...feedback,
@@ -36,15 +39,38 @@ const Form = () => {
     initialFeedback
   );
 
-  
   return (
-    <div className="form">
-      <h3>Contact Us</h3>
-      <input placeholder="Name*" type="text" onChange={handleName}></input>
-      <input placeholder="Email*" type="email" onChange={handleEmail}></input>
-      <input placeholder="Telephone" type="tel" onChange={handlePhone}></input>
-      <textarea placeholder="Message*" onChange={handleMessage}></textarea>
-      <button onClick={handleSubmit}>Submit</button>
+    <div>
+      {!feedback.valid ? (
+        <div className="form">
+          <h3>Contact Us</h3>
+          <input placeholder="Name*" type="text" onChange={handleName}></input>
+          <input
+            placeholder="Email*"
+            type="email"
+            onChange={handleEmail}
+          ></input>
+          <input
+            placeholder="Telephone"
+            type="tel"
+            onChange={handlePhone}
+          ></input>
+          <textarea placeholder="Message*" onChange={handleMessage}></textarea>
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+      ) : (
+        <div className="form">
+            <h5>Thank you for your feedback, {feedback.name}!</h5>
+          <p>
+             A Shop Shopper will be
+            in touch with you shortly. <strong>Your confirmation number is #
+            {Math.floor(Math.random() * 10000000)}.</strong> Please write it down and burn
+            it under the next new moon for best results. In the meantime, why not stop on by
+            the Shop Shop shop and see what fun tchotchkes or world-ending knickknacks we've got
+            in store for your shopping cart this time?
+          </p>
+        </div>
+      )}
     </div>
   );
 };
